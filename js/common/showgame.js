d19d88b360db49655280285ba0d81c0da7d74435
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-11-26 02:23:52 +0100

    Update showgame.js
    check@23

--- js/common/showgame.js
+++ js/common/showgame.js
@@ -8,21 +8,27 @@ export class ShowGame extends InitGame {
     this.obj = obj;
     this._btnMap;
     this._container;
-    this._cardArr = cardArr;
+    this.cardArr = cardArr;
   }
 
   mapContainer(container) {
     //debugger;
-    const crd = this._cardArr.map(
+    const crd = this.cardArr.map(
       elm => common.elementCreator(...elm)
     );
-
+    let appender;
     crd.forEach((elm, ind) => {
       const mapElem = this._btnMap.get(Array.from(this._btnMap.keys(k => k))[ind]);
-      const cA = card.cardAdder(elm, mapElem, this._cardArr[ind][2]);
-      const appender = (ind === 1)?
-        common.elementCreator("div", null, "card-adder-test").append(cA):
-        cA;
+      const cA = card.cardAdder(elm, mapElem, this.cardArr[ind][2]);
+      if(ind === 1) {
+        const cat = common.elementCreator("div", null, ["card-adder-test"]);
+        appender = cat.append(cA);
+      } else {
+        appender = cA;
+      }
+      // appender = (ind === 1)? 
+      //   common.elementCreator("div", null, ["card-adder-test"]).append(cA):
+      //   cA;
 
       // debugger;
       // container.append(card.cardAdder(elm, mapElem, this._cardArr[ind][2]));
